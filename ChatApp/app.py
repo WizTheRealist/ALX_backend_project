0c89a8e4ba71c9673951cf9b352a9ff4cd894a7f
Dafinci01 <Davehacker382gmail.com>
2025-01-01 07:32:43 +0100

    temporary

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 from flask_socketio import SocketIO, emit
 import random
-import redis
+#import redis
 
 app = Flask(__name__)
 socketio = SocketIO(app)
